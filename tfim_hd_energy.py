a74d31f1a83511bd2abe1290cd869ec9f85c79fd
Jack Landrigan <jlandrigan@middlebury.edu>
2020-07-02 02:37:18 -0400

    Updated files 07/02/2020

---
+++ tfim_hd_energy.py
@@ -0,0 +1,82 @@
+import tfim
+import numpy as np
+
+#################################################################################
+
+def ground_states(N, energy):
+    """returns list of ground state configuration indices for a random instance of 
+    N-spin system"""
+    energy1 = energy.tocsr()
+    ground = [0]
+    for i in range(1,2**N):
+        if energy1[i,i] > energy1[ground[0],ground[0]]:
+            ground = [i]
+        elif energy1[i,i] == energy1[ground[0],ground[0]]:
+            ground.append(i)
+    return ground
+
+#################################################################################
+
+def hamming_distance(basis, ind1, ind2):
+    """returns hamming distance between two configurations of an N-spin system"""
+    state1 = basis.state(ind1)
+    state2 = basis.state(ind2)
+    nonzero = np.nonzero(state1-state2)
+    ham_dist = len(nonzero[0])
+    return ham_dist
+
+#################################################################################
+
+def all_ham_dist(N, basis, ind):
+    """returns list of hamming distance values between configuration given by index 
+    and all other configurations in N-spin system"""
+    hd_list = []
+    for i in range(2**N):
+        hd_list.append(hamming_distance(basis, ind, i))
+    return hd_list
+
+#################################################################################
+
+def energy_states(N, energy):
+    """returns list of which energy state each configuration of N-spin system is in"""
+    energy1 = energy.tocsr()
+    values = []
+    states = []
+    for i in range(2**N):
+        if energy1[i,i] not in values:
+            values.append(energy1[i,i])
+            values.sort(reverse=True)
+    for j in range(2**N):
+        index = values.index(energy1[j,j])
+        states.append(index)
+    return states
+
+#################################################################################
+
+def hamming_energy_array(N, basis, energy, ind):
+    """returns array with count of configurations at given energy level and hamming distance 
+    from configuration given by ind (ex: if there are 6 configs in energy level 3 at hamming 
+    distance 4, array[3,4] = 6)"""
+    states = energy_states(N, energy)
+    hd_list = all_ham_dist(N, basis, ind)
+    dim1 = max(states) +1
+    dim2 = max(hd_list) +1
+    arr = np.empty([dim1,dim2])
+    arr.fill(0)
+    for i in range(len(hd_list)):
+        ind1 = states[i]
+        ind2 = hd_list[i]
+        arr[ind1,ind2] +=1
+    return(arr)
+
+#################################################################################
+
+def ground_arrays(N, basis, energy):
+    """returns list containing a hamming distance/energy level array for each ground
+    state configuration"""
+    ground_list = ground_states(N, energy)
+    array_list = []
+    for i in range(len(ground_list)):
+        arr = hamming_energy_array(N, basis, energy, ground_list[i])
+        array_list.append(arr)
+    return array_list
